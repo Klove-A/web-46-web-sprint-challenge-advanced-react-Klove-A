908aee138706ec5a19adfade19a80fc12740af20
Klove Adams <kalebsxott@gmail.com>
2021-09-03 15:22:02 -0400

    Set initial state in the useForm hook

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,17 @@
+import { useState } from "react";
+
 // write your custom hook here to control your checkout form
+
+const initialValue = {
+  firstName: "",
+  lastName: "",
+  address: "",
+  city: "",
+  state: "", 
+  zip: "",
+}; 
+
+const useForm = () => {
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [values, setValues] = useState(initialValue);
+}
